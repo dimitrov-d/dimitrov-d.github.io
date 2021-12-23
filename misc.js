14b94d02d26d3fce3ab1d06d7efd35efaaa80c74
Damjan <damjandimitrov1@gmail.com>
2021-12-23 19:45:31 +0100

    Add ADA.lovelace bot project to portfolio

---
+++ misc.js
@@ -0,0 +1,18 @@
+const tempText = document.getElementById('bottext');
+const projectText = tempText.innerHTML;
+const collapseAnchor = document.getElementById('collapseAnchor');
+tempText.innerHTML = croppedText(projectText);
+
+function expandText() {
+    tempText.innerHTML = projectText;
+    collapseAnchor.style.display = 'block';
+}
+
+function collapseText() {
+    tempText.innerHTML = croppedText(projectText);
+    collapseAnchor.style.display = 'none';
+}
+
+function croppedText(text) {
+    return `${text.substring(0, 100)}... <a class="action-link" onclick="expandText()">Show More</a>`;
+}
